87fb9b167b70dd9a78080e9f0d06b1d15448e635
serafyn <95699669+Marlenephe@users.noreply.github.com>
2023-02-17 20:25:39 -0300

    Primer push

---
+++ main.js
@@ -0,0 +1,15 @@
+function pintar1(col) {
+    document.getElementById('lnk1').style.backgroundColor = col;
+}
+function pintar2(col) {
+
+    document.getElementById('lnk2').style.backgroundColor = col;
+}
+function pintar3(col) {
+    document.getElementById('lnk3').style.backgroundColor = col;
+}
+function pintar4(col) {
+    document.getElementById('lnk4').style.backgroundColor = col;
+}
+
+
